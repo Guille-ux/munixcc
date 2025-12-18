17fd9983436814c216aefea43a66581b6798b69b
Guille-Ux <guillermoleiratemes@protonmail.com>
2025-12-18 18:38:55 +0000

    Adding global and local symbol table

--- include/compat.h
+++ include/compat.h
@@ -5,15 +5,16 @@
  * Esto debe incluir las declaraciones de:
  *
  * Malloc y Free
- * memcpy, memset y strcmp
+ * memcpy, memset, strcmp, strncmp y strlen
  *
  * (NOTA: No uso nada de FOPEN etc, por compatibilidad, a cambio
  *  el ensamblador devolvera los BLOBS binarios directamente)
+ *  Lo saque del código de mi ensamblador como comprenderas
  *
  */
 
 #include <stdlib.h> // para malloc y free
-#include <string.h> // para memcpy, memset y strcmp
+#include <string.h> // para memcpy, memset, strcmp, strncmp y strlen
 #include <ctype.h> // necesario
 
 #endif
--- src/symbols.c
+++ src/symbols.c
@@ -1,1 +1,60 @@
 #include "../include/symbols.h"
+
+MCC_g_Table CGlobalTable;
+MCC_VarTable CVarTable;
+
+// TODO: Cambiar las asignaciones directas de structs por memcpy
+
+void mcc_add_g(MCC_Var var) {
+	if (CGlobalTable.symbol_count >= MCC_MAX_SYMBOLS) {
+		// ERROR
+		return;
+	}
+	var.global = true;
+	CGlobalTable.table[CGlobalTable.symbol_count++] = var;
+}
+
+void mcc_push_var(MCC_Var var) {
+	if (CVarTable.symbol_count >= MCC_MAX_SYMBOLS) {
+		// ERROR
+		return;
+	}
+	CVarTable.current_stack_offset += var.size;
+	var.nscope = CVarTable.current_scope;
+	var.offset = CVarTable.current_stack_offset;
+	var.global = false;
+	CVarTable.table[CVarTable.symbol_count++] = var;
+}
+
+void mcc_clean_tab(void) {
+	size_t removed = 0;
+	size_t restore_offset = 0;
+	MCC_Var *var;
+	for (size_t i=CVarTable.symbol_count-1;i>0;i--) {
+		var = &CVarTable.table[i];
+		if (var->nscope > CVarTable.current_scope) {
+			restore_offset += var->size;
+			removed++;
+		}
+	}
+	CVarTable.symbol_count -= removed;
+	CVarTable.current_stack_offset -= restore_offset;
+}
+
+MCC_Var *mcc_find_var(char *name) {
+	MCC_VarTable *vtable = &CVarTable;
+	MCC_g_Table *gtable = &CGlobalTable;
+	size_t len = strlen(name);
+	len = (len < MCC_MAX_SYMBOL_NAME) ? len : MCC_MAX_SYMBOL_NAME;
+	for (size_t i=vtable->symbol_count-1;i>0;i--) {
+		if (strncmp(name, vtable->table[i].name, len)==0) {
+			return &vtable->table[i];
+		}
+	}
+	for (size_t i=gtable->symbol_count-1;i>0;i--) {
+		if (strncmp(name, gtable->table[i].name, len)==0) {
+			return &gtable->table[i];
+		}
+	}
+	return (MCC_Var*)NULL;
+} 
